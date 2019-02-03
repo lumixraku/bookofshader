00b41dee4cf839042013041325a5014f45abc358
raku <lumixraku@gmail.com>
2019-02-03 18:34:46 +0800

    add polar shape
    Change-Id: I1ba972d554c7ad4c5b07e36f8379087cf6028048

--- bookofshader05/bookofshader05_step.glsl
+++ bookofshader05/bookofshader05_step.glsl
@@ -6,15 +6,13 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 uniform float u_time;
-float lineWidth = 0.2;
+float lineWidth = 0.05;
 float plot(vec2 st, float pct){
   return  smoothstep( pct-lineWidth, pct, st.y) -
           smoothstep( pct, pct+lineWidth, st.y);
 }
 
-void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution;
-
+vec3 fromLeft2Right(vec2 st){
     // Step will return 0.0 unless the value is over 0.5,
     // in that case it will return 1.0
     // step() 阶跃函数  step(x, v)  当 v > x 返回1  否则0 
@@ -22,9 +20,34 @@ void main() {
     float y = step(0.3,st.x);
 
     vec3 color = vec3(y);
-
     float pct = plot(st,y);
+    
+
     color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
 
+
+    return color;
+}
+
+vec3 fromBottom2Top(vec2 st){
+    float x = step(0.3,st.y);
+
+    vec3 color = vec3(x);
+    float pct = plot(st,x);
+    
+    
+    color = (1.0-pct)*color  +  pct*vec3(0.0,1.0,0.0);
+
+
+    return color;    
+}
+
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    // glsl的笛卡尔坐标系从原点在左下角  右上角是(1,1)
+    
+    // vec3 color = fromLeft2Right(st);
+    vec3 color = fromBottom2Top(st);
     gl_FragColor = vec4(color,1.0);
 }
--- bookofshader07/bookofshader07_polarshape.glsl
+++ bookofshader07/bookofshader07_polarshape.glsl
@@ -12,19 +12,30 @@ uniform float u_time;
 void main(){
     float blur = 0.2;
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    // glsl的笛卡尔坐标系从原点在左下角  右上角是(1,1)
+
     vec3 color = vec3(0.0);
+ 
+    vec2 pos = vec2(0.5)-st; // 将原点移动到画布正中心sddsdfs颠三倒四返回回复sss
+    pos.x = pos.x + 0.05;// 默认有点偏左
 
-    vec2 pos = vec2(0.5)-st;
+    // 首先将笛卡尔坐标系转为 极坐标  用length 得到极坐标的r 用arctan得到极坐标的θ
+    float r = length(pos)*2.0; //r的长度为实际值的2倍 这使得后面使用step的时候感觉是在一个(-2,2)的画布上
+    float angle = atan(pos.y,pos.x);  // 通过arctan 求出角度
 
-    float r = length(pos)*2.0;
-    float a = atan(pos.y,pos.x);
+    float f = cos(angle*3.); // *3表示3个半圆
+    // f = abs(cos(angle*3.));
+    // f = abs(cos(angle*2.5))*.5+.3;
+    f = abs(cos(angle*12.)*sin(angle*3.))*.8+.1;
+    // f = smoothstep(-.5,1., cos(angle*10.))*0.2+0.5;
 
-    float f = cos(a*3.);
-    f = abs(cos(a*3.));
-    f = abs(cos(a*2.5))*.5+.3;
-    // f = abs(cos(a*12.)*sin(a*3.))*.8+.1;
-    // f = smoothstep(-.5,1., cos(a*10.))*0.2+0.5;
+    // step() 阶跃函数  step(x, v)  当 v > x 返回1  否则0 
+    // 设置 f = cos(angle*1.); 感受下step的变化
+    color = vec3( step(0.4,r) );
+    color = vec3( step(f,0.71) ); //θ接近45° //google 搜索 cos 45degree得到 0.7
 
+
+    color = vec3( step(f,r) );
     color = vec3( 1.-smoothstep(f,f+blur,r) );
 
     gl_FragColor = vec4(color, 1.0);
